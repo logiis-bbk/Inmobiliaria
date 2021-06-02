5888117935ba937c89044a28cde860610088db3f
Diego Sierra <nirvana-ponal@hotmail.com>
2021-06-01 22:25:09 -0500

    creación de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,47 @@
+/*
+  _                   ,  ,    ___  
+ | |                 (_)(_) (  __ |              
+ | |      _       __  |  |   \ \ \|          
+ | |   :/ _ \  :/ _ \| | | :  \ \        
+ | |   | | | | | |_| | || ||\__\ \       
+ | |___| |_| |  \__  |_||_||____,_)      
+ |____/ \___/ , __| |              
+                | _ /                                   
+                      
+  Desarrollado por Logi is  
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//angular material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+//Componentes
+import { LoginComponent } from './login/login.component';
 import { HeadboardComponent } from './headboard/headboard.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadboardComponent
+    HeadboardComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
